104111cf26b8eb051c2f723c37f5a610c53446bf
Keerthi Suresh <keerthisureshd@gmail.com>
2026-03-16 15:08:20 +0530

    Use Case 2: Basic Room Types & Static Availability

--- BookMyStay/src/BookMyStayApp.java
+++ BookMyStay/src/BookMyStayApp.java
@@ -1,9 +1,71 @@
+abstract class Room{
+    protected int noOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int noOfBeds,int squareFeet,double pricePerNight){
+        this.noOfBeds=noOfBeds;
+        this.squareFeet=squareFeet;
+        this.pricePerNight=pricePerNight;
+    }
+    public void displayRoomDetails(){
+        System.out.println("----Room details----");
+        System.out.println("Number of bed in room : "+noOfBeds+"\ntotal room size : "+squareFeet+" sq ft\ncost per night : Rs."+pricePerNight);
+    }
+}
+
+class SingleRoom extends Room{
+    public static int availability = 5;
+    public SingleRoom(){
+        super(1,25,1500.0);
+    }
+
+    @Override
+    public void displayRoomDetails() {
+        System.out.println("- Single Room -");
+        super.displayRoomDetails();
+        System.out.println("Availablity : "+availability);
+    }
+}
+
+class DoubleRoom extends Room{
+    public static int availability = 3;
+    public DoubleRoom(){
+        super(2,400,2500.0);
+    }
+    @Override
+    public void displayRoomDetails() {
+        System.out.println("- Double Room -");
+        super.displayRoomDetails();
+        System.out.println("Availablity : "+availability);
+    }
+}
+
+class SuiteRoom extends Room{
+    public static int availability = 2;
+    public SuiteRoom(){
+        super(3,750,5000.0);
+    }
+    @Override
+    public void displayRoomDetails() {
+        System.out.println("\n- Suite Room -");
+        super.displayRoomDetails();
+        System.out.println("Availablity : "+availability);
+    }
+}
+
 public class BookMyStayApp {
     /*
-     * version 1.0
+     * version 2.0
      * author @Keerthi
      */
     public static void main (String[] args) {
-        System.out.println("Welcome to the Hotel Management Booking System\nSystem initialised successfully.");
+        SingleRoom sr1 = new SingleRoom();
+        DoubleRoom dr1 = new DoubleRoom();
+        SuiteRoom su1 = new SuiteRoom();
+
+        sr1.displayRoomDetails();
+        dr1.displayRoomDetails();
+        su1.displayRoomDetails();
     }
 }
