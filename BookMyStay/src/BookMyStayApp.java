f69fab996bfd7496364dc76c3cfc9858e030eb8d
Keerthi Suresh <keerthisureshd@gmail.com>
2026-04-02 20:53:14 +0530

    Use Case 3: Centralized Room Inventory Management

--- BookMyStay/src/BookMyStayApp.java
+++ BookMyStay/src/BookMyStayApp.java
@@ -1,7 +1,43 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class RoomInventory{
+    private Map<String,Integer> roomAvailability;
+
+    public RoomInventory(){
+        initializeInventory();
+    }
+
+    public void initializeInventory(){
+        roomAvailability= new HashMap<>();
+        roomAvailability.put("single",6);
+        roomAvailability.put("double",4);
+        roomAvailability.put("suite",2);
+    }
+
+    public Map<String,Integer> getRoomAvailability(){
+        return roomAvailability;
+    }
+
+    public void updateAvailability(String roomType,int count){
+        roomAvailability.put(roomType,count);
+    }
+
+    public void bookRoom(String roomType){
+        int available = roomAvailability.getOrDefault(roomType,0);
+        roomAvailability.put(roomType,available-1);
+    }
+    public void unbookRoom(String roomType){
+        int available = roomAvailability.getOrDefault(roomType,0);
+        roomAvailability.put(roomType,available+1);
+    }
+}
+
 abstract class Room{
     protected int noOfBeds;
     protected int squareFeet;
     protected double pricePerNight;
+    RoomInventory inventory = new RoomInventory();
 
     public Room(int noOfBeds,int squareFeet,double pricePerNight){
         this.noOfBeds=noOfBeds;
@@ -22,9 +58,9 @@ class SingleRoom extends Room{
 
     @Override
     public void displayRoomDetails() {
-        System.out.println("- Single Room -");
+        System.out.println("\n- Single Room -");
         super.displayRoomDetails();
-        System.out.println("Availablity : "+availability);
+        System.out.println("Availability : "+inventory.getRoomAvailability().get("single"));
     }
 }
 
@@ -35,9 +71,9 @@ class DoubleRoom extends Room{
     }
     @Override
     public void displayRoomDetails() {
-        System.out.println("- Double Room -");
+        System.out.println("\n- Double Room -");
         super.displayRoomDetails();
-        System.out.println("Availablity : "+availability);
+        System.out.println("Availability : "+inventory.getRoomAvailability().get("double"));
     }
 }
 
@@ -50,22 +86,21 @@ class SuiteRoom extends Room{
     public void displayRoomDetails() {
         System.out.println("\n- Suite Room -");
         super.displayRoomDetails();
-        System.out.println("Availablity : "+availability);
+        System.out.println("Availability : "+inventory.getRoomAvailability().get("suite"));
     }
 }
 
 public class BookMyStayApp {
     /*
-     * version 2.0
+     * version 3.0
      * author @Keerthi
      */
     public static void main (String[] args) {
-        SingleRoom sr1 = new SingleRoom();
-        DoubleRoom dr1 = new DoubleRoom();
-        SuiteRoom su1 = new SuiteRoom();
-
-        sr1.displayRoomDetails();
-        dr1.displayRoomDetails();
-        su1.displayRoomDetails();
+        SingleRoom singleRoom = new SingleRoom();
+        DoubleRoom doubleRoom = new DoubleRoom();
+        SuiteRoom suiteRoom = new SuiteRoom();
+        singleRoom.displayRoomDetails();
+        doubleRoom.displayRoomDetails();
+        suiteRoom.displayRoomDetails();
     }
 }
